3376f640ed3b65c46d516847493a7eeac81a717b
tom <tom20191219@gmail.com>
2020-04-08 13:45:28 +0800

    枚举类

--- c/c_exercise/main.cpp
+++ c/c_exercise/main.cpp
@@ -2,13 +2,26 @@
 
 int intPlusTwoInt(int *intA, int *intB);
 
+enum class Weekday : int;
+
+enum class Weekday : int {
+    MONDAY = 1,
+    TUESDAY = 2,
+    WEDNESDAY = 3,
+    THURSDAY = 4,
+    FRIDAY = 5,
+    SATURDAY = 6,
+    SUNDAY = 7
+};
+
 int main() {
     int
             intA,
             intB,
             *pointerIntA = &intA,
             *pointerIntB = &intB;
-
+    intA = static_cast<int>(Weekday::MONDAY);
+    intB = static_cast<int>(Weekday::TUESDAY);
     printf("the result of intA plus intB:\n");
     printf("please input intA:\n");
     scanf("%d", &intA);
@@ -16,8 +29,8 @@ int main() {
     scanf("%d", &intB);
     printf("intA:%d\n", intA);
     printf("intB:%d\n", intB);
-    printf("pointerIntA:%d\n",pointerIntA);
-    printf("pointerIntB:%d\n",pointerIntB);
+    printf("pointerIntA:%d\n", pointerIntA);
+    printf("pointerIntB:%d\n", pointerIntB);
     printf("intAintB:%d\n", intPlusTwoInt(pointerIntA, pointerIntB));
 
     return 0;
